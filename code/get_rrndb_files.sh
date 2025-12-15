0fad65968e0b401427baa1a3959bd2af7546a18c
RichStack <91222271+RichStack@users.noreply.github.com>
2025-12-15 15:06:19 +0000

    Remove obsolete folders

--- code/get_rrndb_files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#Author: Richard Stack
-# Inputs: Name of the file extracted from the archive (without the path)
-#Outputs: The appropriate file into data/raw
-
-archive=$1
-
-wget -nc -P data/raw https://rrndb.umms.med.umich.edu/static/download/"$archive".zip
-unzip -n -d data/raw data/raw/"$archive".zip
